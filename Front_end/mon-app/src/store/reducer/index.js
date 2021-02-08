17d5b29b3656dac94e829dafc09231f2b83ad027
farahbe <fbechoual@gmail.com>
2021-02-08 14:37:12 +0100

    index reducer ajout

--- Front_end/mon-app/src/store/reducer/index.js
+++ Front_end/mon-app/src/store/reducer/index.js
@@ -1,12 +1,14 @@
 import {combineReducers} from 'redux';
 import articlereducer from './ajout_article';
 import adminreducer from './admin';
+import categoriereducer from './categories';
 
 
 export default combineReducers(
      {
         articlereducer,
         adminreducer,
+        categoriereducer,
 
     }
 )
