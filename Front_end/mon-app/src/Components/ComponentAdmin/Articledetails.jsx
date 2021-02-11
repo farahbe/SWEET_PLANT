faa8d4035e8902829abe91bb2073e9b88229d883
farahbe <fbechoual@gmail.com>
2021-02-11 14:38:38 +0100

    creation article details components

---
+++ Front_end/mon-app/src/Components/ComponentAdmin/Articledetails.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+class articlePage extends Component{
+    state = {
+        article: {}
+    }
+
+render () {
+    return(
+        <div>
+            <h1>Voici l'article selectionner</h1>
+
+                    <Card className="text-center">
+                    <Card.Header>Featured</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Special title treatment</Card.Title>
+                        <Card.Text>
+                        With supporting text below as a natural lead-in to additional content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                    <Card.Footer className="text-muted">2 days ago</Card.Footer>
+                    </Card>
+        </div>
+    )
+}
+
+
+
+}
+
+export default articlePage;
+
+
+
