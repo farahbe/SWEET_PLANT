7fe323d92b0af6decb5213a04b52da379ca98acc
farahbe <fbechoual@gmail.com>
2021-02-22 20:44:29 +0100

    reparer bug article details ligne 89

--- Front_end/mon-app/src/Components/ComponentAdmin/Articledetails.jsx
+++ Front_end/mon-app/src/Components/ComponentAdmin/Articledetails.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+//store
+import {connect} from 'react-redux'
+import {enregistrecommentaire} from '../../store/action/commentaires'
 
 class articlePage extends Component {
     state = {
@@ -10,7 +13,6 @@ class articlePage extends Component {
     }
 
     componentDidMount() {
-        console.log(this);
 
         const {id} = this.props.match.params 
         // On passe les props a la const id_article
@@ -23,11 +25,13 @@ class articlePage extends Component {
          // element recoit les data de lobjet correspondant a lID envoyer 
 
         })
+        
+        this.setState({ commentaire: this.props.usercommentaires })
 
         axios.get(`http://localhost:4000/user/postcomments`)
         .then(res => {
             console.log(res.data);
-            this.setState({commentaire: res.data[0] });
+            this.props.commentaires(res.data)
          // element recoit les data de lobjet correspondant a lID envoyer 
 
         })
@@ -60,16 +64,18 @@ render() {
       
     )}
 
-    {this.state.commentaire && (
-        <div>
+    {this.state.commentaire && this.props.commentaire.map((elem, i) => {
+
+        return(
+        <div key={elem.id_article}>
             <h1>commentaires</h1>
 
                 <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={this.state.commentaire.avatar} />
+                <Card.Img variant="top" src={elem.avatar} />
                 <Card.Body>
-                    <Card.Title>{this.state.commentaire.pseudo}</Card.Title>
+                    <Card.Title>{elem.pseudo}</Card.Title>
                     <Card.Text>
-                    {this.state.commentaire.commentaire}
+                    {elem.commentaire}
                     </Card.Text>
                     {/* <Button variant="primary">Go somewhere</Button> */}
                 </Card.Body>
@@ -90,7 +96,18 @@ render() {
 
 }
 
-export default articlePage;
+const mapStateToProps = (state /*, ownPrps*/) => {
+    //mapStateToProps permet de parcourir les props
+
+    return {
+        commentaire: state.commentairereducer.commentaire
+    }
+}
+
+ const mapDispatchToProps = {enregistrecommentaire}
+    //mapDispatchToProps dispatche l'action a toutes les props
+
+export default connect(mapStateToProps)(articlePage );
 
 
 
--- Front_end/mon-app/src/store/reducer/index.js
+++ Front_end/mon-app/src/store/reducer/index.js
@@ -2,13 +2,14 @@ import {combineReducers} from 'redux';
 import articlereducer from './ajout_article';
 import adminreducer from './admin';
 import categoriereducer from './categories';
-
+import commentairereducer from './commentaires';
 
 export default combineReducers(
      {
         articlereducer,
         adminreducer,
         categoriereducer,
+        commentairereducer,
 
     }
 )
